eccf7c6af882c0bdae027fe987f28b838cf5e697
fungzzz <100516229+fungzzz@users.noreply.github.com>
2022-10-09 23:52:36 +0800

    21212
    12121212121

--- flutter_application_1/lib/LoginPages/forgot-password.dart
+++ flutter_application_1/lib/LoginPages/forgot-password.dart
@@ -22,135 +22,140 @@ class _ForgotPageState extends State<ForgotPage> {
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
       body: SafeArea(
         child: Center(
-          child: Column(children: [
-            SizedBox(height: 44),
-            Text(
-              'B u l i d  U P',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 53.0,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Forgot password?',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
-                fontSize: 22.0,
-              ),
-            ),
-            SizedBox(height: 23.0),
-            Text(
-              '',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
-                fontSize: 19.0,
-              ),
-            ),
-
-            Column(
-              children: [
-                Image.network(
-                  urlImage,
-                  height: 200,
-                  width: 200,
-                )
-              ],
-            ),
-
-            SizedBox(height: 25.0),
-
-            //Email TextField,
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    'Email',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 17.0,
-                    ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 145),
+              child: Column(children: [
+                SizedBox(height: 44),
+                Text(
+                  'B u l i d  U P',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 53.0,
                   ),
                 ),
-                SizedBox(height: 7),
+                SizedBox(height: 20.0),
+                Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 22.0,
+                  ),
+                ),
+                SizedBox(height: 23.0),
+                Text(
+                  '',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 19.0,
+                  ),
+                ),
+
+                Column(
+                  children: [
+                    Image.network(
+                      urlImage,
+                      height: 200,
+                      width: 200,
+                    )
+                  ],
+                ),
+
+                SizedBox(height: 25.0),
+
+                //Email TextField,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Text(
+                        'Email',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 181, 156, 255),
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Colors.black,
+                            ),
+                            hintText: "Enter your email",
+                            hintStyle: TextStyle(
+                                fontSize: 17.0,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                //Message(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Text(
+                        '* We will send you a message to set or reset   your new password',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50),
+
+                //SendEmail()
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 181, 156, 255),
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(
-                          Icons.email,
-                          color: Colors.black,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(360, 50),
+                          backgroundColor: Color.fromARGB(255, 181, 156, 255),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50))),
+                      onPressed: () {},
+                      child: Text(
+                        'Send Email',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
                         ),
-                        hintText: "Enter your email",
-                        hintStyle: TextStyle(
-                            fontSize: 17.0,
-                            color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                     ),
                   ),
                 ),
-              ],
+              ]),
             ),
-
-            SizedBox(height: 10),
-
-            //Message(),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    '* We will send you a message to set or reset   your new password',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 17.0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 50),
-
-            //SendEmail()
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(360, 50),
-                      backgroundColor: Color.fromARGB(255, 181, 156, 255),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50))),
-                  onPressed: () {},
-                  child: Text(
-                    'Send Email',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ]),
+          ),
         ),
       ),
     );
