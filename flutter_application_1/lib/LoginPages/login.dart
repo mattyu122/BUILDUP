518fd1cecdfc8ae6810973533931580498e442c8
fungzzz <100516229+fungzzz@users.noreply.github.com>
2022-10-09 22:39:40 +0800

    2121
    212121

--- flutter_application_1/lib/LoginPages/login.dart
+++ flutter_application_1/lib/LoginPages/login.dart
@@ -23,243 +23,241 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
       body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
-            child: Column(children: [
-              SizedBox(height: 44),
-              Text(
-                'B u l i d  U P',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 53.0,
-                ),
+          child: Column(children: [
+            SizedBox(height: 44),
+            Text(
+              'B u l i d  U P',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 53.0,
               ),
-              SizedBox(height: 20.0),
-              Text(
-                'Welcome Back!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 22.0,
-                ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Welcome Back!',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                fontSize: 22.0,
               ),
-              SizedBox(height: 23.0),
-              Text(
-                'Please login to continue',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 19.0,
-                ),
+            ),
+            SizedBox(height: 23.0),
+            Text(
+              'Please login to continue',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                fontSize: 19.0,
               ),
-              SizedBox(height: 45),
+            ),
+            SizedBox(height: 45),
 
-              //Email TextField,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Text(
-                      'Email',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 7),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 181, 156, 255),
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.email,
-                            color: Colors.black,
-                          ),
-                          hintText: "Enter your email",
-                          hintStyle: TextStyle(
-                              fontSize: 17.0,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 38),
-
-              //Password TextField,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Text(
-                      'Password',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 7),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 181, 156, 255),
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                HidePassword = !HidePassword;
-                              });
-                            },
-                            child: Icon(HidePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                          ),
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: Colors.black,
-                          ),
-                          hintText: "Enter your password",
-                          hintStyle: TextStyle(
-                              fontSize: 17.0,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        obscureText: HidePassword,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              // BoxForgot()
-              Container(
-                alignment: Alignment.centerRight,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ForgotPage()),
-                    );
-                  },
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+            //Email TextField,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Text(
-                    'Forgot Password?',
+                    'Email',
                     style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.normal,
-                      fontSize: 14.0,
+                      fontSize: 17.0,
                     ),
                   ),
                 ),
-              ),
-
-              //Check box Remember Me
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 0),
-                    child: CheckboxListTile(
-                        controlAffinity: ListTileControlAffinity.leading,
-                        side: MaterialStateBorderSide.resolveWith(
-                          (states) => BorderSide(
-                              width: 1.0,
-                              color: Color.fromARGB(255, 255, 255, 255)),
+                SizedBox(height: 7),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 181, 156, 255),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.black,
                         ),
-                        title: Text(
-                          'Remember me',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.normal,
-                              fontSize: 16.0),
-                        ),
-                        value: isChecked,
-                        activeColor: Colors.black,
-                        onChanged: (newBool) {
-                          setState(() {
-                            isChecked = newBool;
-                          });
-                        }),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 28),
-              // Sign IN
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(360, 50),
-                        backgroundColor: Color.fromARGB(255, 181, 156, 255),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50))),
-                    onPressed: () {},
-                    child: Text(
-                      'SIGN IN',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
+                        hintText: "Enter your email",
+                        hintStyle: TextStyle(
+                            fontSize: 17.0,
+                            color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                     ),
                   ),
                 ),
-              ),
+              ],
+            ),
 
-              SizedBox(height: 38),
-              // Sign Up
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(360, 50),
-                        backgroundColor: Color.fromARGB(255, 181, 156, 255),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50))),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()),
-                      );
-                    },
-                    child: Text(
-                      'SIGN UP',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
+            SizedBox(height: 38),
+
+            //Password TextField,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Text(
+                    'Password',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 17.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 7),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 181, 156, 255),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              HidePassword = !HidePassword;
+                            });
+                          },
+                          child: Icon(HidePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                        ),
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                        ),
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(
+                            fontSize: 17.0,
+                            color: Color.fromARGB(255, 0, 0, 0)),
                       ),
+                      obscureText: HidePassword,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            // BoxForgot()
+            Container(
+              alignment: Alignment.centerRight,
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ForgotPage()),
+                  );
+                },
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+            ),
+
+            //Check box Remember Me
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 0),
+                  child: CheckboxListTile(
+                      controlAffinity: ListTileControlAffinity.leading,
+                      side: MaterialStateBorderSide.resolveWith(
+                        (states) => BorderSide(
+                            width: 1.0,
+                            color: Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                      title: Text(
+                        'Remember me',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16.0),
+                      ),
+                      value: isChecked,
+                      activeColor: Colors.black,
+                      onChanged: (newBool) {
+                        setState(() {
+                          isChecked = newBool;
+                        });
+                      }),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 28),
+            // Sign IN
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(360, 50),
+                      backgroundColor: Color.fromARGB(255, 181, 156, 255),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                  onPressed: () {},
+                  child: Text(
+                    'SIGN IN',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
               ),
-            ]),
-          ),
+            ),
+
+            SizedBox(height: 38),
+            // Sign Up
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(360, 50),
+                      backgroundColor: Color.fromARGB(255, 181, 156, 255),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterPage()),
+                    );
+                  },
+                  child: Text(
+                    'SIGN UP',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
